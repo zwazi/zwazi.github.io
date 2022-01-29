e08ae146bf3faeeaf17f449440ba7093fc53474c
zwazi <zwsaz-github@yahoo.com>
2022-01-29 11:25:32 -0500

    getting there

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,17 +1,18 @@
 /* Opens the navbar */
 function openNav() {
-  document.getElementById("sidebar-iframe").style.left = "0";
-  document.getElementById("capsule-logo-spacer").style.zIndex = "0";
-  document.getElementById("sidebar-iframe").style.zIndex = "3";
-  document.getElementById("sidebar-background-filter").style.zIndex = "4";
-  document.getElementById("sidebar-background-filter").style.left = "350px";
+  document.getElementById("sidebar").style.display = "block"
+  document.getElementById("sidebar").style.left = "0"
+  document.getElementById("sidebar-background-filter").style.left = "0"
 }
 
 /* Closes the nav bar */
 function closeNav() {
-  document.getElementById("sidebar-iframe").style.left = "-350px";
-  document.getElementById("capsule-logo-spacer").style.zIndex = "3";
-  document.getElementById("sidebar-iframe").style.zIndex = "0";
-  document.getElementById("sidebar-background-filter").style.zIndex = "0";
-  document.getElementById("sidebar-background-filter").style.left = "-100%";
+  document.getElementById("sidebar").style.display = "none"
+  document.getElementById("sidebar").style.left = "-1000"
+  document.getElementById("sidebar-background-filter").style.left = "-100%"
 } 
+
+function openChild() {
+  document.getElementById("child-table").style.display = "table"
+  document.getElementsByClassName("scan-table-body-expand-button-vertical")[0].style.display = "none";
+}
